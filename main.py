a1964a3980d473a5c14009a845f4b3fd437d07e1
AlexArguetaNet <alexarguetanet@gmail.com>
2025-10-16 14:07:37 -0400

    Added requests package

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import os
 from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
 def main():
-    ...
+    print(getForecast()["weather"])
+    
+def getForecast():
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q=charlotte,us&units=imperial&appid={api_key}")
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return False
 
 if __name__ == "__main__":
-    main()
+    main() 
