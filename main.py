a9ff9848dc6353f5479f1bd3929afcd3bc975f34
AlexArguetaNet <alexarguetanet@gmail.com>
2025-10-20 14:31:00 -0400

    Added more ascii art to art.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     if not curr_forecast:
         print("An error occured. Please check your internet connection.")
     else:
-        print(art.partly_cloudy)
+        print(art.snow)
         print(curr_forecast["weather"])
     
 def get_forecast(location):
