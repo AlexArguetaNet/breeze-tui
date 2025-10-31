f013da169f2d2e9dc1a25a1ba16702117e6f0eb5
AlexArguetaNet <alexarguetanet@gmail.com>
2025-10-30 20:04:38 -0400

    Completed forecast module

--- modules/forecast.py
+++ modules/forecast.py
@@ -1,3 +1,51 @@
+import art
+
 class Forecast:
     def __init__(self, weather):
-        ...
+        self.city = weather["city"]
+        self.desc = weather["desc"]
+        self.temp = weather["temp"]
+        self.wind = weather["wind"]
+        self.ascii = self.set_ascii()
+
+    @property
+    def city(self):
+        return self._city
+    @city.setter
+    def city(self, city):
+        self._city = city
+
+    @property
+    def desc(self):
+        return self._desc
+    @desc.setter
+    def desc(self, desc):
+        self._desc = desc
+
+    @property
+    def temp(self):
+        return self._temp
+    @temp.setter
+    def temp(self, temp):
+        self._temp = temp
+    
+    @property
+    def wind(self):
+        return self._wind
+    @wind.setter
+    def wind(self, wind):
+        self._wind = wind
+
+    def set_ascii(self):
+        print(self.desc)
+        match self.desc:
+            case "clear sky":
+                return art.sun
+            case "scattered clouds":
+                return art.sun_and_cloud
+            case _:
+                return "image"
+            
+    def __str__(self):
+        return f"{self.ascii}\n{self.temp}\n{self.desc}"
+
