fb86214888c744a0845eaadd6a842a735c62fa9d
AlexArguetaNet <alexarguetanet@gmail.com>
2025-10-30 20:04:38 -0400

    Completed forecast module

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 from dotenv import load_dotenv
 import requests
 from util import get_curr_location, set_curr_location
-import art
-
+from modules.forecast import Forecast
 load_dotenv()
 api_key = os.getenv("API_KEY")
+
 
 def main():
     curr_location = get_curr_location()
@@ -17,15 +17,23 @@ def main():
     if not curr_forecast:
         print("An error occured. Please check your internet connection.")
     else:
-        print(art.snow)
-        print(curr_forecast["main"][0])
+        print(get_forecast(curr_location))
+
+
     
 def get_forecast(location):
     response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={location["city"]},{location["country"]}&units=imperial&appid={api_key}")
     if response.status_code == 200:
         data = response.json()
 
-        return data
+        weather = {
+            "city": data["name"],
+            "desc": data["weather"][0]["description"],
+            "temp": data["main"]["temp"],
+            "wind": data["wind"]
+        }
+
+        return Forecast(weather)
     else:
         return None
     
