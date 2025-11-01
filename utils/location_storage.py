0cc41c53ebedb57a2eab53ee23a1ba294edc0f7d
AlexArguetaNet <alexarguetanet@gmail.com>
2025-10-31 20:59:31 -0400

    Rewrote set_curr_location to inclue optional location parameter

--- utils/location_storage.py
+++ utils/location_storage.py
@@ -12,14 +12,19 @@ def get_curr_location():
     except FileNotFoundError:
         return None
     
-def set_curr_location():
+def set_curr_location(location=""):
     try:
-        city = input("Enter your city: ")
-        country = input("Enter country code: ")
-        if city and country:
+        if location == "":
+            city = input("Enter your city: ")
+            country = input("Enter country code: ")
+            if city and country:
+                with open("curr_location.txt", "w") as file:
+                    file.write(f"{city},{country}")
+            return {"city": city, "country": country}
+        else:
             with open("curr_location.txt", "w") as file:
-                file.write(f"{city},{country}")
-        return {"city": city, "country": country}
+                    file.write(f"{location["city"]},{location["country"]}")
+            
 
     except ValueError:
         return None
--- utils/weather_api.py
+++ utils/weather_api.py
@@ -3,17 +3,39 @@ from modules.forecast import Forecast
 from utils.env_config import api_key
 
 def get_forecast(location):
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={location["city"]},{location["country"]}&units=imperial&appid={api_key}")
-    if response.status_code == 200:
-        data = response.json()
+    try:
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={location["city"]},{location["country"]}&units=imperial&appid={api_key}")
+        if response.status_code == 200:
+            data = response.json()
 
-        weather = {
-            "city": data["name"],
-            "desc": data["weather"][0]["description"],
-            "temp": data["main"]["temp"],
-            "wind": data["wind"]
-        }
+            weather = {
+                "city": data["name"],
+                "desc": data["weather"][0]["description"],
+                "temp": data["main"]["temp"],
+                "wind": data["wind"]
+            }
 
-        return Forecast(weather)
-    else:
-        return None
+            return {"cod": 200, "forecast": Forecast(weather)}
+        else:
+            return response.json()
+        
+    except requests.exceptions.ConnectionError as e:
+        return e
+    
+    
+
+# def get_forecast(location):
+#     response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={location["city"]},{location["country"]}&units=imperial&appid={api_key}")
+#     if response.status_code == 200:
+#         data = response.json()
+
+#         weather = {
+#             "city": data["name"],
+#             "desc": data["weather"][0]["description"],
+#             "temp": data["main"]["temp"],
+#             "wind": data["wind"]
+#         }
+
+#         return Forecast(weather)
+#     else:
+#         return None
